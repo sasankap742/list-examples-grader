27acac66f035cd2cfc482d53c9f4e947b0f1e654
sasankap742 <78668680+sasankap742@users.noreply.github.com>
2022-11-10 09:53:15 -0800

    testStringChecker

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,55 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
+
 
 public class TestListExamples {
   // Write your grading tests here!
+  ListExamples newStudentExample = new ListExamples();
+
+  @Test
+  public void testMerge2ListsInOrder(){
+    List<String> list1 = new ArrayList<>(Arrays.asList("a","b","c"));
+
+    List<String> list2 = new ArrayList<>(Arrays.asList("d","e", "f"));
+
+    List<String> result = newStudentExample.merge(list1,list2);
+    
+    List<String> expected =  new ArrayList<>(Arrays.asList("a","b","c","d","e", "f"));
+    assertEquals(expected,result);
+  }
+
+  @Test
+  public void testMerge2ListsOutOfOrder(){
+    List<String> list1 = new ArrayList<>(Arrays.asList("a","c","e"));
+
+    List<String> list2 = new ArrayList<>(Arrays.asList("b","d", "f"));
+
+    List<String> result = newStudentExample.merge(list1,list2);
+    
+    List<String> expected =  new ArrayList<>(Arrays.asList("a","b","c","d","e", "f"));
+    assertEquals(expected,result);
+  }
+
+  @Test
+  public void testMergeListsDuplicateReferenceItems(){
+    String a = "a";
+    List<String> list1 = new ArrayList<>(Arrays.asList("a","b","c"));
+
+    List<String> list2 = new ArrayList<>(Arrays.asList(a,"d", "e"));
+
+    List<String> result = newStudentExample.merge(list1,list2);
+    
+    List<String> expected =  new ArrayList<>(Arrays.asList("a",a,"b","c","d","e"));
+    assertEquals(expected,result);
+  }
+
+
+
+
+
+
+
+
+
 }
--- grade.sh
+++ grade.sh
@@ -5,3 +5,10 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
+#junit jar
+CP = "lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
+
+javac -cp $CP*.java
+
+java -cp $CP org.junit.runner.JunitCore TestListExamples
+
