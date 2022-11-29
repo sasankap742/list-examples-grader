a4c037f2a5a285e30cc0242401d4ba14a86deedb
sasankap742 <78668680+sasankap742@users.noreply.github.com>
2022-11-28 23:40:05 -0800

    final

--- TestListExamples.java
+++ TestListExamples.java
@@ -7,6 +7,15 @@ public class TestListExamples {
   // Write your grading tests here!
   ListExamples newStudentExample = new ListExamples();
 
+  public class allUpperCase implements StringChecker {
+          public boolean checkString(String s){
+                  if(s.equals(s.toUpperCase())){
+                          return true;
+                  }
+                  return false;
+          }
+  }
+
   @Test
   public void testMerge2ListsInOrder(){
     List<String> list1 = new ArrayList<>(Arrays.asList("a","b","c"));
@@ -14,10 +23,11 @@ public class TestListExamples {
     List<String> list2 = new ArrayList<>(Arrays.asList("d","e", "f"));
 
     List<String> result = newStudentExample.merge(list1,list2);
-    
+
     List<String> expected =  new ArrayList<>(Arrays.asList("a","b","c","d","e", "f"));
     assertEquals(expected,result);
   }
+
 
   @Test
   public void testMerge2ListsOutOfOrder(){
@@ -26,7 +36,7 @@ public class TestListExamples {
     List<String> list2 = new ArrayList<>(Arrays.asList("b","d", "f"));
 
     List<String> result = newStudentExample.merge(list1,list2);
-    
+
     List<String> expected =  new ArrayList<>(Arrays.asList("a","b","c","d","e", "f"));
     assertEquals(expected,result);
   }
@@ -39,13 +49,22 @@ public class TestListExamples {
     List<String> list2 = new ArrayList<>(Arrays.asList("d","e", "f"));
 
     List<String> result = newStudentExample.merge(list1,list2);
-    
+
     List<String> expected =  new ArrayList<>(Arrays.asList(a,"b","c","d","e", "f"));
     assertEquals(expected,result);
   }
 
+  @Test
+  public void testFilter(){
+          List<String> list1 = new ArrayList<>(Arrays.asList("APPLE","orange","PEAR"));
+          List<String> expected = new ArrayList<>(Arrays.asList("APPLE","PEAR"));
+
+          assertEquals(expected,newStudentExample.filter(list1, new allUpperCase()));
+  }
 
 
+
+}
 
 
 
